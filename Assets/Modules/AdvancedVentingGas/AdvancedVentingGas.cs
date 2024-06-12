11efaa255a0a53d9611d9125cf40d10ded483d0c
Hexicube <marc@firstlinetech.co.uk>
2024-06-12 19:11:57 +0100

    Added questions to Needy Quiz
    Also tweaked self-reference question to be more clear

--- Assets/Modules/AdvancedVentingGas/AdvancedVentingGas.cs
+++ Assets/Modules/AdvancedVentingGas/AdvancedVentingGas.cs
@@ -11,9 +11,15 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Text;
 
 public class AdvancedVentingGas : MonoBehaviour
 {
+    private char[] Serial = null;
+    private int Batteries = -1;
+
+    private static int TEST_MODE_IDX = -1; // -1 to disable
+
     public static int loggingID = 1;
     public int thisLoggingID;
 
@@ -30,11 +36,13 @@ public class AdvancedVentingGas : MonoBehaviour
     protected bool HasReply;
     protected System.Func<AdvancedVentingGas, bool, bool> CurQ;
 
-    protected bool DidHakuna = false;
     protected bool LastWasSelfReference = false;
+    protected bool EverMadeMistake = false;
+    protected bool EverSaidYes = false;
+    protected bool EverSaidNo = false;
     protected bool Exploded = false;
 
-    protected bool abortMode = false;
+    protected string abortMode = null;
     protected bool forceSolve = false;
 
     private int NumHasReply = 3;
@@ -42,18 +50,32 @@ public class AdvancedVentingGas : MonoBehaviour
     private List<KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>> QuestionList = new List<KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>>(){
         {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("What was your\nprevious answer?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;if (Module.HasReply) return Response == Module.LastReply;return true;})},
         {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("What wasn't your\nprevious answer?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;if (Module.HasReply) return Response == !Module.LastReply;return true;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Was the last\nanswered question\nabout the last\ngiven answer?", delegate(AdvancedVentingGas Module, bool Response){bool Correct = Response == Module.LastWasSelfReference;Module.LastWasSelfReference = true;if (Module.HasReply) return Correct;return true;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Was the last\nanswered question\nabout a previous\nquestion or answer?", delegate(AdvancedVentingGas Module, bool Response){bool Correct = Response == Module.LastWasSelfReference;Module.LastWasSelfReference = true;if (Module.HasReply) return Correct;return true;})},
         
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\n3 lines?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\n6 lines?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return !Response;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("At least\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() > 0) == Response;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("More than\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() > 1) == Response;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Up to\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() < 2) == Response;})},
-        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Less than\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() < 1) == Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Have you previously\nanswered Yes\nto a question?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;return Response == Module.EverSaidYes;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Have you previously\nanswered No\nto a question?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;return Response == Module.EverSaidNo;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Have you previously\nanswered not Yes\nto a question?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;return Response == Module.EverSaidNo;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Have you previously\nanswered not No\nto a question?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;return Response == Module.EverSaidYes;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Have you previously\nanswered a question\nincorrectly?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = true;return Response == Module.EverMadeMistake;})},
+        
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\nthree lines?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\nsix lines?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return !Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\nthree words?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return !Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does this\nquestion contain\nsix words?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response;})},
+
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Do you have\nat least\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() > 0) == Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Do you have\nmore than\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() > 1) == Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Do you have\nup to\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() < 2) == Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Do you have\nless than\n1 strike?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() < 1) == Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Do you have\nmore strikes\nthan batteries?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return (Module.BombInfo.GetStrikes() > Module.GetBatteries()) == Response;})},
+        
         {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Abort?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return !Response;})},
-        //{new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Is \"Hakuna Matata\"\na wonderful phrase?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;Module.DidHakuna = true;return Response;})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("SEGFAULT", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response;})},
+        
         {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Are you a\ndirty cheater?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return !Response;})},
+        
         {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does the\nserial contain\nduplicate\ncharacters?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response == Module.SerialDuplicate();})},
+        {new KeyValuePair<string, System.Func<AdvancedVentingGas, bool, bool>>("Does the number of\nbatteries and the\nsum of serial number\ndigits parities match?", delegate(AdvancedVentingGas Module, bool Response){Module.LastWasSelfReference = false;return Response == Module.CheckParity();})},
     };
 
     void Awake()
@@ -78,21 +100,37 @@ public class AdvancedVentingGas : MonoBehaviour
 
         YesButton.GetComponent<MeshRenderer>().material.color = new Color(0.91f, 0.88f, 0.86f);
         NoButton.GetComponent<MeshRenderer>().material.color = new Color(0.91f, 0.88f, 0.86f);
+
+        if (TEST_MODE_IDX != -1) NewQuestion();
     }
 
     protected bool HandleYes()
     {
-        if(abortMode) return false;
-        if (DidHakuna || CurQ != null) YesButton.AddInteractionPunch();
+        if (TEST_MODE_IDX != -1) {
+            TEST_MODE_IDX = (TEST_MODE_IDX + 1) % QuestionList.Count;
+            NewQuestion();
+            return false;
+        }
+
+        if (abortMode != null) return false;
+        if (CurQ != null) YesButton.AddInteractionPunch();
         HandleResponse(true);
+        EverSaidYes = true;
         return false;
     }
 
     protected bool HandleNo()
     {
-        if(abortMode) return false;
-        if (DidHakuna || CurQ != null) NoButton.AddInteractionPunch();
+        if (TEST_MODE_IDX != -1) {
+            TEST_MODE_IDX = (TEST_MODE_IDX + QuestionList.Count - 1) % QuestionList.Count;
+            NewQuestion();
+            return false;
+        }
+
+        if (abortMode != null) return false;
+        if (CurQ != null) NoButton.AddInteractionPunch();
         HandleResponse(false);
+        EverSaidNo = true;
         return false;
     }
 
@@ -102,62 +140,49 @@ public class AdvancedVentingGas : MonoBehaviour
             GetComponent<KMNeedyModule>().HandlePass();
             return;
         }
-        if (abortMode) return;
-        if (DidHakuna) Display.text = "Is \"Hakuna Matata\"\na passing craze?";
+        if (abortMode != null) return;
         else NewQuestion();
     }
 
     protected void OnNeedyDeactivation()
     {
-        if (abortMode) return;
+        if (TEST_MODE_IDX != -1) return;
+
+        if (abortMode != null) return;
         CurQ = null;
         Display.text = "";
     }
 
     protected void HandleResponse(bool R)
     {
-        if (DidHakuna && !Display.text.Equals(""))
+        if (CurQ == null) return;
+        Debug.Log("[Answering Questions #"+thisLoggingID+"] Quiz: " + Display.text.Replace("\n", " "));
+        Debug.Log("[Answering Questions #"+thisLoggingID+"] Given answer: " + (R ? "Y" : "N"));
+        if (CurQ(this, R))
         {
-            Debug.Log("[Answering Questions #"+thisLoggingID+"] Quiz: Is \"Hakuna Matata\" a passing craze?");
-            Debug.Log("[Answering Questions #"+thisLoggingID+"] Given answer: " + (R ? "Y" : "N"));
-            DidHakuna = false;
-            if (R)
+            Debug.Log("[Answering Questions #"+thisLoggingID+"] Answer was correct");
+            Sound.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, gameObject.transform);
+        }
+        else
+        {
+            if (Display.text.Equals("Abort?"))
+            {
+                Debug.Log("[Answering Questions #"+thisLoggingID+"] ABORT! ABORT!!! ABOOOOOOORT!!!!!");
+                abortMode = "ABORT!";
+            }
+            else if (Display.text.StartsWith("SEGFAULT")) {
+                Debug.Log("[Answering Questions #"+thisLoggingID+"] SEGFAULT (ABORT!)");
+                abortMode = "SEGFAULT!";
+            }
+            else
             {
                 Debug.Log("[Answering Questions #"+thisLoggingID+"] Answer was incorrect");
                 GetComponent<KMNeedyModule>().HandleStrike();
             }
-            else
-            {
-                Debug.Log("[Answering Questions #"+thisLoggingID+"] Answer was correct");
-                Sound.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, gameObject.transform);
-            }
-            GetComponent<KMNeedyModule>().HandlePass();
+            EverMadeMistake = true;
         }
-        else
-        {
-            if (CurQ == null) return;
-            Debug.Log("[Answering Questions #"+thisLoggingID+"] Quiz: " + Display.text.Replace("\n", " "));
-            Debug.Log("[Answering Questions #"+thisLoggingID+"] Given answer: " + (R ? "Y" : "N"));
-            if (CurQ(this, R))
-            {
-                Debug.Log("[Answering Questions #"+thisLoggingID+"] Answer was correct");
-                Sound.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, gameObject.transform);
-            }
-            else
-            {
-                if (Display.text.Equals("Abort?"))
-                {
-                    Debug.Log("[Answering Questions #"+thisLoggingID+"] ABORT! ABORT!!! ABOOOOOOORT!!!!!");
-                    abortMode = true;
-                }
-                else
-                {
-                    Debug.Log("[Answering Questions #"+thisLoggingID+"] Answer was incorrect");
-                    GetComponent<KMNeedyModule>().HandleStrike();
-                }
-            }
-            GetComponent<KMNeedyModule>().HandlePass();
-        }
+        GetComponent<KMNeedyModule>().HandlePass();
+
         HasReply = true;
         LastReply = R;
         CurQ = null;
@@ -165,21 +190,48 @@ public class AdvancedVentingGas : MonoBehaviour
     }
 
     private float ticker = 0f;
+    private float segTicker = 0f;
     void FixedUpdate() {
-        if(!forceSolve && abortMode && !Exploded) {
+        segTicker += Time.fixedDeltaTime;
+        if (segTicker > 0.5f) {
+            segTicker -= 0.5f;
+            if (Display.text.StartsWith("SEGFAULT")) {
+                StringBuilder builder = new StringBuilder();
+                builder.Append("SEGFAULT\n");
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                builder.Append("\n");
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                builder.Append("\nContinue?");
+                Display.text = builder.ToString();
+            }
+            else if (abortMode == "SEGFAULT!") {
+                StringBuilder builder = new StringBuilder();
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                builder.Append("\n");
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                builder.Append("\n");
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                builder.Append("\n");
+                for (int a = 0; a < 10; a++) builder.Append((char)Random.Range((int)'A', (int)'Z'+1));
+                Display.text = builder.ToString();
+            }
+        }
+        if(!forceSolve && abortMode != null && !Exploded) {
             bool state = ticker >= 1f;
             ticker += Time.fixedDeltaTime;
-            if(state) {
+            if (state) {
                 if(ticker >= 2f) {
                     ticker -= 2f;
-                    Display.text = "";
+                    if (abortMode != "SEGFAULT!") Display.text = "";
                 }
             }
             else {
                 if(ticker >= 1f) {
-                    Service.CauseStrike("ABORT!");
-                    Display.text = "ABORT!";
-                    Display.fontSize = 380;
+                    Service.CauseStrike(abortMode);
+                    if (abortMode != "SEGFAULT!") {
+                        Display.text = abortMode;
+                        Display.fontSize = 380;
+                    }
                 }
             }
         }
@@ -187,15 +239,12 @@ public class AdvancedVentingGas : MonoBehaviour
 
     protected void OnTimerExpired()
     {
-        if (forceSolve || (CurQ == null && !DidHakuna)) return;
-        DidHakuna = false;
+        if (TEST_MODE_IDX != -1) return;
+        if (forceSolve || CurQ == null) return;
         GetComponent<KMNeedyModule>().HandleStrike();
     }
 
-    private char[] Serial = null;
-
-    private bool SerialDuplicate()
-    {
+    private void SetSerialVariable() {
         if (Serial == null)
         {
             List<string> data = BombInfo.QueryWidgets(KMBombInfo.QUERYKEY_GET_SERIAL_NUMBER, null);
@@ -206,7 +255,23 @@ public class AdvancedVentingGas : MonoBehaviour
                 break;
             }
         }
+    }
 
+    private int GetBatteries() {
+        if (Batteries == -1) {
+            List<string> data = BombInfo.QueryWidgets(KMBombInfo.QUERYKEY_GET_BATTERIES, null);
+            foreach (string response in data)
+            {
+                Dictionary<string, int> responseDict = JsonConvert.DeserializeObject<Dictionary<string, int>>(response);
+                Batteries += responseDict["numbatteries"];
+            }
+        }
+        return Batteries;
+    }
+
+    private bool SerialDuplicate()
+    {
+        SetSerialVariable();
         List<char> list = new List<char>();
         foreach (char c in Serial)
         {
@@ -217,8 +282,25 @@ public class AdvancedVentingGas : MonoBehaviour
         return false;
     }
 
+    private bool CheckParity() {
+        SetSerialVariable();
+        bool answer = true;
+        foreach (char c in Serial) {
+            if (c == '1' || c == '3' || c == '5' || c == '7' || c == '9') answer = !answer;
+        }
+        if (GetBatteries() % 2 == 1) answer = !answer;
+        return answer;
+    }
+
     protected void NewQuestion()
     {
+        if (TEST_MODE_IDX != -1) {
+            CurQ = QuestionList[TEST_MODE_IDX].Value;
+            Display.text = QuestionList[TEST_MODE_IDX].Key;
+            if (Display.text == "SEGFAULT") segTicker = 0.11f;
+            return;
+        }
+
         int val = 0;
         for (int i = 0; i < NumTriesForAbort; i++) {
             if(HasReply) val = Random.Range(0, QuestionList.Count);
@@ -227,6 +309,7 @@ public class AdvancedVentingGas : MonoBehaviour
         }
         CurQ = QuestionList[val].Value;
         Display.text = QuestionList[val].Key;
+        if (Display.text == "SEGFAULT") segTicker = 0.51f;
     }
 
     //Twitch Plays support
